4c4dd64611f1be72d4eb9c14016cd7b42ce068a8
codifin <108587219+codifin@users.noreply.github.com>
2022-07-03 18:10:22 +0500

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,18 @@
+token = '5540190664:AAH-N74uvKtekpVpwmXkllo-mh0NKFGZs44'
+
+minrandom = 0
+maxrandom = 1
+randomresult = 0
+
+blacklist = 1
+
+newschannel = 'obupdate'
+adminusername = 'codifin'
+logschannel = 'oblogs'
+logschannelid = -1001549558838
+
+jokesaddress = 'https://www.generatormix.com/random-jokes'
+quotesaddress = 'http://bash.org/?random'
+
+saymuchcycles = 20
+
